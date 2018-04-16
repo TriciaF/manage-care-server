84daa3cbd5bf550d22111b083e5bcb6f6888ca81
TriciaF <TriciaF@thinkful.com>
2018-04-16 12:34:01 -0400

    fix for depoyment

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://TriciaF:tjandsam01
 exports.TEST_DATABASE = 'mongodb://localhost/test-datatbase';
 
 exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://manage-care.netlify.com';
 exports.JWT_SECRET = 'I_MISS_SAM';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
