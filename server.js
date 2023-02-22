22a9b58b4ab5384f5a9db43aa3f8cb29d871c990
TriciaF <palforrester@gmail.com>
2023-02-22 13:50:44 -0500

    add OK response to ORIGINS

--- server.js
+++ server.js
@@ -29,13 +29,9 @@ app.use(function(req, res, next)
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
 	if (req.method === 'OPTIONS') {
-		return res.send(204);
+		return res.sendStatus(200)
 	}
 	next();
-});
-
-app.options('/*', (_,res) => {
-	res.sendStatus(200);
 });
 
 passport.use(localStrategy);
